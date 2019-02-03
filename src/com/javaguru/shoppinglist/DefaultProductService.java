724da2faeaa2e2ff7345e94dd89d1b9965fce3af
owned <owned.by.dz@gmail.com>
2019-02-03 10:24:49 +0200

    prodcut validator: price, discount, name Product class: added category, discount, description

--- src/com/javaguru/shoppinglist/DefaultProductService.java
+++ src/com/javaguru/shoppinglist/DefaultProductService.java
@@ -1,5 +1,6 @@
 package com.javaguru.shoppinglist;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,6 +8,8 @@ public class DefaultProductService implements ProductService {
 
     private Map<Long, Product> database = new HashMap<>();
     private Long PRODUCT_ID_SEQUENCE = 0L;
+
+    private BigDecimal validPrice = new BigDecimal(0);
 
     public Product findBy(Long id) {
         if (id == null) {
@@ -19,6 +22,14 @@ public class DefaultProductService implements ProductService {
     public Long create(Product product) {
         if (product == null) {
             throw new IllegalArgumentException("Cannot be null");
+        } else if (product.getPrice().compareTo(validPrice) <= 0){
+            throw new IllegalArgumentException("Price cannot be less or equal to 0");
+        } else if (product.getDiscount() > 100L){
+            throw new IllegalArgumentException("Discount cannot be more than 100 %");
+        } else if (product.getName().length() >= 32 || product.getName().length() <= 3){
+            throw new IllegalArgumentException("Name is too short/long");
+        }else if (product.getDiscount() < 0L) {
+            throw new IllegalArgumentException("Discount cannot be less than 0");
         }
         product.setId(PRODUCT_ID_SEQUENCE);
 
